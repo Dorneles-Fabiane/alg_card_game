a1c90180017c427b14e7b00fe36ad05da1605f94
Fabiane_Dorneles <64361469+Dorneles-Fabiane@users.noreply.github.com>
2020-07-06 12:30:38 -0300

    update

--- js/script.js
+++ js/script.js
@@ -6,80 +6,49 @@ var vComputer = [];
 var deck = new Array();
 
 //Create an array to unite the cards and suits.
-function getDeck()
-{
+function getDeck() {
 
 	for(var posS = 0; posS < suits.length; posS++)
 	{
-        //populate the deck Array with the objects (cards + suits)
+    //populate the deck Array with the objects (cards + suits)
 		for(var posC = 0; posC < cards.length; posC++)
 		{
 			var card = (cards[posC] + suits[posS]); 
-            deck.push(card);
+      deck.push(card);
 		}
 	}
 	return deck;
 }
+///////////////////////////////////////////////////////////////////////////
+
 
 //Select a card randomly and show it on the screen
-function selectCard()
-{
+function selectCard () {
+
+  var index = getRandomNumber(0, deck.length-1);
+
   playerCardImg = document.querySelector('div#playerCardImg');
   playerCardImg.style.textAlign = 'center';
   var imgP = document.createElement('img');
-  imgP.setAttribute('id','cards');
+  imgP.setAttribute('id','img');
   imgP.setAttribute('width',172);
   imgP.setAttribute('height',264);
 
-  var card = getRndInteger(1,13);
-  var naipe = makeid (1);
-  var cards = card + naipe;
-  playerCardImg.innerHTML = "Player";
-  imgP.setAttribute('src', 'img/' + cards + '.png');
-
-  deck.splice(deck.indexOf(cards), 1);
-
+  var card = deck[index];
+  
+  computerCardImg.innerHTML = card;
+  var showimg = imgP.setAttribute('src', 'img/' + card + '.png');
   playerCardImg.appendChild(imgP);
-  vPlayer.push(card) //Add de value of the card to the players array
 
-
-    computerCardImg = document.querySelector('div#computerCardImg');
-    computerCardImg.style.textAlign = 'center';
-    var imgC = document.createElement('img');
-    imgC.setAttribute('id','cards');
-    imgC.setAttribute('width',172);
-    imgC.setAttribute('height',264);
-  
-    var card = getRndInteger(1,13);
-    var naipe = makeid (1);
-    var cards = card + naipe;
-    computerCardImg.innerHTML = "Computer";
-    imgC.setAttribute('src', 'img/' + cards + '.png');
-  
-    deck.splice(deck.indexOf(cards), 1);
-  
-    computerCardImg.appendChild(imgC);
-    vComputer.push(card) //Add de value of the card to the computer array
-  
+  deck.splice(deck.indexOf(showimg), 1);
+  vComputer.push(card) //Add the value of the card to the players array
 }
 
-function getRndInteger(min, max) {
+function getRandomNumber(min, max) { 
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-function makeid(length) {
-    var playerCardImgult = '';
-    var characters = 'CDHS';
-    var charactersLength = characters.length;
-    
-    for ( var cont = 0; cont < length; cont++ ) {
-       playerCardImgult = characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return playerCardImgult;
- }
-
- function load()
-{
+function load() {
 	deck = getDeck();
 }
 
